0165548776d4326ffb4190a67d135fe5753dd39e
Olaoluwa Sanya <soconsults@gmail.com>
2022-09-22 21:23:49 -0700

    100-rot13.c let us see

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,30 +1,46 @@
 #include "main.h"
+
 /**
- *rot13 - encoding a string using rot13.
- *@s: the string to be encoded to rot13.
- *Return: the string s encoded to rot13.
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
  */
+char *rot13(char *str)
+{
+	int indx1 = 0, indx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+				'G', 'H', 'I', 'J', 'K', 'L',
+				'M', 'N', 'O', 'P', 'Q', 'R',
+				'S', 'T', 'U', 'V', 'W', 'X',
+				'Y', 'Z', 'a', 'b', 'c', 'd',
+				'e', 'f', 'g', 'h', 'i', 'j',
+				'k', 'l', 'm', 'n', 'o', 'p',
+				'q', 'r', 's', 't', 'u', 'v',
+				'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+				'T', 'U', 'V', 'W', 'X', 'Y',
+				'Z', 'A', 'B', 'C', 'D', 'E',
+				'F', 'G', 'H', 'I', 'J', 'K',
+				'L', 'M', 'n', 'o', 'p', 'q',
+				'r', 's', 't', 'u', 'v', 'w',
+				'x', 'y', 'z', 'a', 'b', 'c',
+				'd', 'e', 'f', 'g', 'h', 'i',
+				'j', 'k', 'l', 'm'};
 
-char *rot13(char *s)
-{int i = 0, j = 0;
-
-	char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
-
-	char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-
-	while (s[i] != '\0')
+	while (str[indx1])
 	{
-		do {
-			if (s[i] == string_alpha[j])
+		for (indx2 = 0; indx2 < 52; indx2++)
+		{
+			if (str[indx1] == alphabet[indx2])
 			{
-				s[i] = string_rot13[j];
+				str[indx1] = rot13key[indx2];
 				break;
 			}
-			j++;
-		} while (string_alpha[j] != '\0');
-	}
-	j = 0;
-	i++;
+		}
 
-	return (s);
+		indx1++;
+
+	}
+	return (str);
 }
