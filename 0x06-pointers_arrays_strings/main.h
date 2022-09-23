654a092297c2c2c3c959195fb87cd33950a5a44f
Olaoluwa Sanya <soconsults@gmail.com>
2022-09-23 00:22:07 -0700

    Edited Header

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,5 +16,6 @@ char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
+void magic(int n);
 
 #endif /* MAIN_H */
