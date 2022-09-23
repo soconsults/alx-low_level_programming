768ff1f72c7900ea9c98f6bbd95ec6d99f045628
Olaoluwa Sanya <soconsults@gmail.com>
2022-09-22 21:33:21 -0700

    101-print_number.c edited

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,7 +11,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar(45);
+		_putchar('-');
 		m = n * -1;
 	}
 	else
@@ -19,18 +19,8 @@ void print_number(int n)
 		m = n;
 	}
 
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	if ((m/10)>0)
+		print_number(m/10);
+	
+	_putchar((m % 10) + '0');
 }
