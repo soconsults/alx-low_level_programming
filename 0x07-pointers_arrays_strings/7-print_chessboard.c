6b2bd4f04a72c83d7e53b80bec3feb288d606920
Olaoluwa Sanya <soconsults@gmail.com>
2022-09-26 21:46:39 -0700

    Task 7 reviewed

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,7 +7,7 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i,j:
+	int i,j;
 
 	for (i = 0; i < 8; i++)
 	{
@@ -15,6 +15,6 @@ void print_chessboard(char (*a)[8])
 		{
 			_putchar(a[i][j]);
 		}
-		_putchar(10)
+		_putchar(10);
 	}
 }
